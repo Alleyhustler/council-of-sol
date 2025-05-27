2d84f7cdc7fdb7a43d57b97ae3702d12157cefbc
Alleyhustler <153684384+Alleyhustler@users.noreply.github.com>
2025-05-27 14:46:16 +0200

    sd

---
+++ components.js
@@ -0,0 +1,14 @@
+$(function(){ 
+      $("#navi").load("https://cyberneticdryad.neocities.org/components/navi/navi.html");
+      $("#currently").load("https://cyberneticdryad.neocities.org/components/currently.html");
+
+      $("#acnavi").load("https://cyberneticdryad.neocities.org/components/navi/animalcrossing.html");
+      $("#danavi").load("https://cyberneticdryad.neocities.org/components/navi/dragonage.html");
+      $("#pknavi").load("https://cyberneticdryad.neocities.org/components/navi/pokemon.html");
+      $("#swnavi").load("https://cyberneticdryad.neocities.org/components/navi/starwars.html");
+
+      $("#craftnavi").load("https://cyberneticdryad.neocities.org/components/navi/crafts.html");
+      $("#kitchnavi").load("https://cyberneticdryad.neocities.org/components/navi/kitchen.html");
+      $("#librarynavi").load("https://cyberneticdryad.neocities.org/components/navi/library.html");
+    });
+  
